5651dd04c59955deb28df5ea08f28896c5aaea5e
johnypeople <35531563+johnypeople@users.noreply.github.com>
2019-07-15 11:34:16 -0300

    Correção de código

--- E and H field in waveguide/GuiasJG.m
+++ E and H field in waveguide/GuiasJG.m
@@ -1,18 +1,15 @@
-%Trabalho de guia de ondas 
-%João Guilherme Domingos de Oliveira 
-
-%Entrada de variáveis
+%Entrada de variÃ¡veis
 clear;
 clc;
-f  = input('Digite a frequência de operação [GHz]: ');
-f = f*1e9;             %Frequência de operação
-a = input('Digite a dimensão "a" do guia (cm): ');
-a = a*1e-2;             %Dimensão a do guia 
-b = input('Digite a dimensão "b" do guia (cm): ');
-b = b*1e-2;             %Dimensão b do guia 
-mod = input('Digite o Modo de propagação 1)- TE 2)- TM :');%Modo de propagação
-m = input('Digite o índice "m": Ex: TEm:  '); %(TE/TM)mn
-n = input('Digite o índice "n": Ex: TEn: ' );%(TE/TM)mn
+f  = input('Digite a frequÃªncia de operaÃ§Ã£o [GHz]: ');
+f = f*1e9;             %FrequÃªncia de operaÃ§Ã£o
+a = input('Digite a dimensÃ£o "a" do guia (cm): ');
+a = a*1e-2;             %DimensÃ£o a do guia 
+b = input('Digite a dimensÃ£o "b" do guia (cm): ');
+b = b*1e-2;             %DimensÃ£o b do guia 
+mod = input('Digite o Modo de propagaÃ§Ã£o 1)- TE 2)- TM :');%Modo de propagaÃ§Ã£o
+m = input('Digite o Ã­ndice "m": Ex: TEm:  '); %(TE/TM)mn
+n = input('Digite o Ã­ndice "n": Ex: TEn: ' );%(TE/TM)mn
 pass = 50;
 
 if mod == 1
@@ -26,18 +23,18 @@ end
 tg = 0.0001;             %Tangente de perdas
 er = 1;                  %Permissividade relativa
 ur = 1;                  %Permeabilidade relativa
-w  = 2*pi*f;             %Frequência angular 
+w  = 2*pi*f;             %FrequÃªncia angular 
 u0 = 4*pi*1e-7;          %Permeabilidade do ar
 e0 = 8.8541878176e-12;   %Permissividade do ar
 ef = e0*er;              %Permissividade efetiva 
 uf = u0*ur;              %Permeabilidade efetiva
-%Cálculo das demais variáveis 
-c = 1/sqrt(uf*ef);                 %Velocidade da onda no dielétrico
-k = w*sqrt(uf*ef);                 %Número de onda global
-kc = pi*sqrt((m/a)^2 + (n/b)^2);   %Número de onda de corte       
-fc = (c/2)*sqrt((m/a)^2 + (n/b)^2);%Frequência de corte
+%CÃ¡lculo das demais variÃ¡veis 
+c = 1/sqrt(uf*ef);                 %Velocidade da onda no dielÃ©trico
+k = w*sqrt(uf*ef);                 %NÃºmero de onda global
+kc = pi*sqrt((m/a)^2 + (n/b)^2);   %NÃºmero de onda de corte       
+fc = (c/2)*sqrt((m/a)^2 + (n/b)^2);%FrequÃªncia de corte
 lambdac = 2*pi/kc;                 %Comprimento de onda de corte
-lambda = f/(3*10^10);              %1/lambda para determinar o número de modos
+lambda = f/(3*10^10);              %1/lambda para determinar o nÃºmero de modos
 
 
 
